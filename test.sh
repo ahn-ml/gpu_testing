085b4564835cf1f0f9f48ce55acb2b419a493857
Jindong Jiang <jdpshq@gmail.com>
2022-12-13 12:22:02 -0500

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Set the working directory
+cd /tmp/
+
+# Clone the repo
+git clone https://github.com/ahn-ml/gpu_testing.git
+
+# Move into the repo directory
+cd gpu_testing
+
+# Create the virtual environment
+python3 -m venv test_gpu_env
+
+# Activate the virtual environment
+source test_gpu_env/bin/activate
+
+# Install the required packages
+pip install -r requirements.txt
+
+# Run the test script
+python test_gpu.py
+
+# Deactivate the virtual environment
+deactivate
+
+# Remove the repo and virtual environment
+rm -rf /tmp/gpu_testing
